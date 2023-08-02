e732e82b56e5d6afaddd5bf092f764304cc495aa
fbisceglia02 <ferruccio3214@gmail.com>
2023-08-02 20:53:05 +0200

    added url

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main() {
+        URL url = new URL("https","www.javatpoint.com","/URL-class");
+        
+    }
+}
